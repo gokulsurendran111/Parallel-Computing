9f4c7296018020bd0e8754e95dc3083e14946db1
Nikhil Anand <nikhil.myindia@gmail.com>
2023-04-11 23:53:22 +0530

    Auxiliary mathematical procedures
    Auxiliary mathematical procedures required by the main program. Same as the previously committed version.

---
+++ 3D_GJK__mpi/math_aux.py
@@ -0,0 +1,31 @@
+import numpy as np
+from numba import njit
+
+
+@njit(cache=True)
+def quatmultiply(q, p):
+    qs = q[0]
+    qv = q[1:4]
+    ps = p[0]
+    pv = p[1:4]
+
+    ret = np.zeros_like(q, dtype='double')
+    ret[0] = ps*qs - np.dot(pv, qv)
+    ret[1:4] = qs*pv + ps*qv + np.cross(qv, pv)
+    return ret
+
+
+@njit(cache=True)
+def quatconj(q):
+    ret = np.copy(q)
+    ret[1:4] = -1.0*ret[1:4]
+    return ret
+
+
+@njit(cache=True)
+def quatrotate(q, v):
+    vq = np.zeros_like(q, dtype='double')
+    vq[1:4] = v
+    ret1 = quatmultiply(q, vq)
+    ret2 = quatmultiply(ret1, quatconj(q))
+    return ret2[1:4]
