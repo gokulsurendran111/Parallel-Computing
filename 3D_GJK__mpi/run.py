965b03e52df1f928a8e836b6c2bcbb405b68cb6d
Nikhil Anand <nikhil.myindia@gmail.com>
2023-05-04 01:12:17 +0530

    Integrated Closest Distance Computation

--- 3D_GJK__mpi/run.py
+++ 3D_GJK__mpi/run.py
@@ -16,26 +16,43 @@ if rank == 0:
 
     print(f"Running PROC = {rank} ...")
 
-    Target = Body(np.array([0.0, 0.0, 0.0,
-                            0.0, 0.0, 0.0,
-                            1.0, 0.0, 0.0, 0.0,
-                            0.0, 0.0, -0.4]), 1)
+    f = open('Body1.txt')
+    cg_b1 = np.array([float(x) for x in next(f).split()])
+    vel_b1 = np.array([float(x) for x in next(f).split()])
+    q_b1 = np.array([float(x) for x in next(f).split()])
+    omeg_b1 = np.array([float(x) for x in next(f).split()])
+    f.close()
 
-    Chaser = Body(np.array([-8.0, 0.0, 0.0,
-                            0.25, 0.0, 0.0,
-                            1.0, 0.0, 0.0, 0.0,
-                            0.0, 0.2, -0.0]), 2)
+    Tstate = np.hstack((cg_b1, vel_b1, q_b1, omeg_b1))
+    Target = Body(Tstate, 2)
+
+    f = open('Body2.txt')
+    cg_b2 = np.array([float(x) for x in next(f).split()])
+    vel_b2 = np.array([float(x) for x in next(f).split()])
+    q_b2 = np.array([float(x) for x in next(f).split()])
+    omeg_b2 = np.array([float(x) for x in next(f).split()])
+    f.close()
+
+    Cstate = np.hstack((cg_b2, vel_b2, q_b2, omeg_b2))
+    Chaser = Body(Cstate, 3)
+
+    f = open('simparam.txt')
+    TMAX = np.array([float(x) for x in next(f).split()])
+    f.close()
 
     t = 0
     dt = 0.01
-    TMAX = 50.0
 
     # Running Dynamics ------------------------
     t1 = time.perf_counter()
-    time_ar, Target_states, Chaser_states = Simulate(Target, Chaser, t, dt, TMAX)
+    time_ar, Target_states, Chaser_states = Simulate(Target, Chaser, t,
+                                                     dt, TMAX)
     t2 = time.perf_counter()
+    print("\n . . . Dynamics Simulation Completed . . . \n")
     print("Dynamics Time Taken : ", t2 - t1)
+    print("\n . . . Collision Detection Started . . . \n")
 
+    sys.stdout.flush()
     # Running Collision Detection ---------------------
 
     proc_breaks = np.linspace(0, time_ar.size, NPROC+1, dtype='int')
@@ -44,135 +61,115 @@ if rank == 0:
         i1 = proc_breaks[irank]
         i2 = proc_breaks[irank + 1]
 
-        comm.send(i1, dest = irank, tag=30*irank + 3)
-        comm.send(i2, dest = irank, tag=30*irank + 4)
+        comm.send(i1, dest=irank, tag=30*irank + 3)
+        comm.send(i2, dest=irank, tag=30*irank + 4)
 
-        comm.Send(time_ar[i1:i2], dest = irank, tag=30*irank + 5)
-        comm.Send(Target_states[i1:i2, :], dest = irank, tag=30*irank + 6)
-        comm.Send(Chaser_states[i1:i2, :], dest = irank, tag=30*irank + 7)
+        comm.Send(time_ar[i1:i2], dest=irank, tag=30*irank + 5)
+        comm.Send(Target_states[i1:i2, :], dest=irank, tag=30*irank + 6)
+        comm.Send(Chaser_states[i1:i2, :], dest=irank, tag=30*irank + 7)
 
-        comm.send(Target.NumConvxObjects, dest = irank, tag=30*irank + 8)
-        comm.send(Chaser.NumConvxObjects, dest = irank, tag=30*irank + 9)
+        comm.send(Target.NumConvxObjects, dest=irank, tag=30*irank + 8)
+        comm.send(Chaser.NumConvxObjects, dest=irank, tag=30*irank + 9)
 
     i1 = proc_breaks[0]
     i2 = proc_breaks[1]
-    t_col, clos_dist, clos_dist_info, flag_B1_B2, id_clos = collison_check(time_ar[i1:i2], 
-                                                                   Target_states[i1:i2, :],
-                                                                   Chaser_states[i1:i2, :],
-                                                                   Target.NumConvxObjects, 
-                                                                   Chaser.NumConvxObjects)
-
+    t_col, clos_dist_hist, index_hist = collison_check(time_ar[i1:i2],
+                                                       Target_states[i1:i2, :],
+                                                       Chaser_states[i1:i2, :],
+                                                       Target.NumConvxObjects,
+                                                       Chaser.NumConvxObjects)
 
     t_col_arr = []
     t_col_arr.append(t_col)
-    
-    clos_dist_arr = []
-    clos_dist_arr.append(clos_dist)
-    
-    flag_B1_B2_arr = []
-    flag_B1_B2_arr.append(np.floor(flag_B1_B2))
-    
-    id_clos_arr = []
-    id_clos_arr.append(id_clos)
-    
-    clos_dist_info_arr = []
-    clos_dist_info_arr.append(clos_dist_info)
+
+    clos_dist_hist_arr = clos_dist_hist
+    index_hist_arr = index_hist
 
     for irank in range(1, NPROC):
-        temp1 = comm.recv(source=irank, tag=irank)
-        t_col_arr.append(temp1)
-        
-        temp2 = comm.recv(source=irank, tag=irank+NPROC)
-        clos_dist_arr.append(temp2)
-        
-        temp3 = np.zeros((3,)).T
-        comm.Recv(temp3, source = irank, tag=irank+2*NPROC)
-        flag_B1_B2_arr.append(np.floor(temp3))
-        
-        temp4 = comm.recv(source=irank, tag=irank+3*NPROC)
-        id_clos_arr.append(temp4)
-        
-        temp5 = np.zeros((temp4,3))
-        comm.Recv(temp5, source = irank, tag=irank+4*NPROC)
-        clos_dist_info_arr.append(temp5)
+        i1 = proc_breaks[irank]
+        i2 = proc_breaks[irank + 1]
 
-    
+        t_col_nproc = comm.recv(source=irank, tag=irank)
+        t_col_arr.append(t_col_nproc)
+
+        NTIMES = int(i2 - i1)
+        clos_dist_nproc = np.zeros(NTIMES)
+        comm.Recv(clos_dist_nproc, source=irank, tag=irank+NPROC)
+        clos_dist_hist_arr = np.hstack((clos_dist_hist_arr, clos_dist_nproc))
+
+        index_hist_nproc = np.zeros((NTIMES, 4), dtype='int')
+        comm.Recv(index_hist_nproc, source=irank, tag=irank+2*NPROC)
+        index_hist_arr = np.vstack((index_hist_arr, index_hist_nproc))
+
     t_anim = min(t_col_arr)
-    id = t_col_arr.index(t_anim)
+    id_closest_dis = np.argmin(clos_dist_hist_arr)
 
-    t_sort_arr1 = []
-    for si in range(0, NPROC):
-        t_sort_arr1.append(flag_B1_B2_arr[si][2]) 
-
-    t_sort_arr2 = []    
-    t_sort_arr2.append(t_sort_arr1)
-    t_sort_arr2.append(clos_dist_arr)
-    
-    t_sort_arr3 = np.transpose(t_sort_arr2)
-    t_sort_arr4 = sorted(t_sort_arr3, key=lambda i: i[0])
-
-    clos_dist_sorted = np.array([])
-    for si2 in range(id+1):
-        clos_dist_sorted = np.concatenate((clos_dist_sorted, t_sort_arr4[si2][1]))
-    
-    if t_anim == np.inf:
-        print('No Collision Detected')
-        print(f'Closest Distance = %5.2f m' % (clos_dist_arr[id]))
-    else:
-        print(f'Collision Detected @ %5.2f s' % (t_anim))
-        print("Final Closest Points index in ECI frame:")
-        clos_dist_points = clos_dist_info_arr[id]
-        sub_body = flag_B1_B2_arr[id]
-        print("Points ", clos_dist_points[:,1], " in Body 1 - sub-body ", sub_body[0]+1,
-              " are close to points ", clos_dist_points[:,2], " in Body 2 - sub-body ", 
-              sub_body[1]+1, " respectively.")
+    print("\n . . . Collision Detection Completed . . . \n")
 
     t3 = time.perf_counter()
     print(" ")
     print("Col. Detect Time Taken : ", t3 - t2)
 
+    b1_subbody = index_hist_arr[id_closest_dis, 0]
+    b1_point = index_hist_arr[id_closest_dis, 2]
+
+    b2_subbody = index_hist_arr[id_closest_dis, 1]
+    b2_point = index_hist_arr[id_closest_dis, 3]
+
+    if t_anim == np.inf:
+        i_col_true = 0
+        print('\n No Collision Detected')
+        print(f'Closest Distance = %5.2f m' %
+              (clos_dist_hist_arr[id_closest_dis]))
+    else:
+        i_col_true = 1
+        print(f'\nCollision Detected @ %5.2f s' % (t_anim))
+        print("ID of Collision Points :")
+        print(f"Body 1 (Target) :: Sub body {b1_subbody}, Point {b1_point}")
+        print(f"Body 2 (Chaser) :: Sub body {b2_subbody}, Point {b2_point}")
+
     sys.stdout.flush()
 
     # Animation ---------------
-    time_animation = time_ar[time_ar <= t_anim + dt]
-    
+    time_animation = time_ar[time_ar <= t_anim]
+
     plot_len = time_animation.shape[0]
     plt.figure(1)
-    plt.plot(time_animation[0:-1], clos_dist_sorted)
-    plt.xlabel('Time, s')
-    plt.ylabel('Closest Distance, m')
-    plt.grid()
+    plt.grid(True)
+    plt.plot(time_animation, clos_dist_hist_arr[0:plot_len], 'b-', lw=2)
+    plt.xlim([0.0, np.ceil(time_animation[-1])])
+    plt.xlabel('Time (s)', fontsize=15)
+    plt.ylabel('Closest Distance (m)', fontsize=15)
     plt.show()
-    
+
+    b1mark = [b1_subbody, b1_point]
+    b2mark = [b2_subbody, b2_point]
     animate(time_animation, Target_states, Chaser_states,
-            Target, Chaser)
+            Target, Chaser, b1mark, b2mark, i_col_true)
 
 else:
     print(f"Running PROC = {rank} ...")
     NPROC = comm.Get_size()
-    i1 = comm.recv(source = 0, tag=30*rank + 3)
-    i2 = comm.recv(source = 0, tag=30*rank + 4)
+    i1 = comm.recv(source=0, tag=30*rank + 3)
+    i2 = comm.recv(source=0, tag=30*rank + 4)
 
     NTIMES = int(i2 - i1)
     time_ar = np.zeros(NTIMES)
     Target_states = np.zeros((NTIMES, 13))
     Chaser_states = np.zeros((NTIMES, 13))
 
-    comm.Recv(time_ar, source = 0, tag=30*rank + 5)
-    comm.Recv(Target_states, source = 0, tag=30*rank + 6)
-    comm.Recv(Chaser_states, source = 0, tag=30*rank + 7)
-    Target_NumConvx = comm.recv(source = 0, tag=30*rank + 8)
-    Chaser_NumConvx = comm.recv(source = 0, tag=30*rank + 9)
+    comm.Recv(time_ar, source=0, tag=30*rank + 5)
+    comm.Recv(Target_states, source=0, tag=30*rank + 6)
+    comm.Recv(Chaser_states, source=0, tag=30*rank + 7)
+    Target_NumConvx = comm.recv(source=0, tag=30*rank + 8)
+    Chaser_NumConvx = comm.recv(source=0, tag=30*rank + 9)
 
-    t_col, clos_dist, clos_dist_info, flag_B1_B2, id_clos = collison_check(time_ar, 
-                                                                  Target_states,
-                                                                  Chaser_states,
-                                                                  Target_NumConvx, 
-                                                                  Chaser_NumConvx)
+    t_col, clos_dist_hist, index_hist = collison_check(time_ar,
+                                                       Target_states,
+                                                       Chaser_states,
+                                                       Target_NumConvx,
+                                                       Chaser_NumConvx)
 
-    # send_clos_info = clos_dist_info[:,1:]
-    comm.send(t_col, dest = 0, tag=rank)
-    comm.send(clos_dist, dest = 0, tag=rank + NPROC)
-    comm.Send(flag_B1_B2, dest = 0, tag=rank + 2*NPROC)
-    comm.send(id_clos, dest = 0, tag=rank + 3*NPROC)
-    comm.Send(clos_dist_info, dest = 0, tag=rank + 4*NPROC)
+    comm.send(t_col, dest=0, tag=rank)
+    comm.Send(clos_dist_hist, dest=0, tag=rank + NPROC)
+    comm.Send(index_hist, dest=0, tag=rank + 2*NPROC)
